03c31d644a81263971edddab4cf958dd06e08641
Всратослав Бурый <vsratobury@gmail.com>
2021-12-04 18:43:31 +0300

    Добавил .gitignore, настроил nvim как EDITOR, удалил fish_variables.

---
+++ fish/conf.d/nvim.fish
@@ -0,0 +1,6 @@
+if not set -q VIMRUNTIME
+    set -Ux VIMRUNTIME /usr/local/share/nvim/runtime
+    set -Ux VIMRC /Users/asherstnev/.config/nvim/init.lua
+    set -Ux VISUAL nvim
+    set -Ux EDITOR nvim
+end
