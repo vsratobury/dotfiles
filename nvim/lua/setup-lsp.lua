9ebce7fb988d08234732a2872406d0bb057b9193
Всратослав Бурый <3831206+vsratobury@users.noreply.github.com>
2022-09-12 23:44:56 +0300

    Добавил просмотр символов в файле с помощью LSP

--- nvim/lua/setup-lsp.lua
+++ nvim/lua/setup-lsp.lua
@@ -7,79 +7,86 @@ map('n', '<leader>l,', ':lua vim.lsp.diagnostic.goto_prev()<cr>', {})
 map('n', '<leader>l;', ':lua vim.lsp.diagnostic.goto_next()<cr>', {})
 map('n', 'K', ':lua vim.lsp.buf.hover()<cr>', {})
 map('n', 'gd', ':lua vim.lsp.buf.definition()<cr>', {})
+map('n', '<C-h>', ':ClangdSwitchSourceHeader<cr>', {})
+map('n', '<F10>', ':SymbolsOutline<cr>', {})
 
 local M = {
-  'neovim/nvim-lspconfig',
-  requires = {
-    'hrsh7th/cmp-nvim-lsp',
-  },
-  config = function()
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-    local runtime_path = vim.split(package.path, ';')
-    table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
+  { 'neovim/nvim-lspconfig',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      local runtime_path = vim.split(package.path, ';')
+      table.insert(runtime_path, "lua/?.lua")
+      table.insert(runtime_path, "lua/?/init.lua")
 
-    for ls, cfg in pairs({
-      cmake = {
-        capabilities = capabilities
-      },
-      clangd = {
-        capabilities = capabilities
-      },
-      gopls = {
-        cmd = { "gopls", "serve" },
-        settings = {
-          gopls = {
-            analyses = {
-              unusedparams = true,
-            },
-            staticcheck = true,
-          },
+      for ls, cfg in pairs({
+        cmake = {
+          capabilities = capabilities
         },
-        capabilities = capabilities
-      },
-      sumneko_lua = {
-        cmd = { "/Users/vsratobury/.local/lualsp/bin/lua-language-server" },
-        settings = {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT',
-              -- Setup your lua path
-              path = runtime_path,
+        clangd = {
+          capabilities = capabilities
+        },
+        gopls = {
+          cmd = { "gopls", "serve" },
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+            },
+          },
+          capabilities = capabilities
+        },
+        sumneko_lua = {
+          cmd = { "/Users/vsratobury/.local/lualsp/bin/lua-language-server" },
+          settings = {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+                diagnostics = {
+                  globals = { "vim" }
+                }
+              },
               diagnostics = {
-                globals = { "vim" }
-              }
-            },
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = { 'vim' },
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-              enable = false,
-            },
-            format = {
-              enable = true,
-              -- Put format options here
-              -- NOTE: the value should be STRING!!
-              defaultConfig = {
-                indent_style = "space",
-                indent_size = "4",
-              }
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+              },
+              workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              -- Do not send telemetry data containing a randomized but unique identifier
+              telemetry = {
+                enable = false,
+              },
+              format = {
+                enable = true,
+                -- Put format options here
+                -- NOTE: the value should be STRING!!
+                defaultConfig = {
+                  indent_style = "space",
+                  indent_size = "4",
+                }
+              },
             },
           },
+          capabilities = capabilities
         },
-        capabilities = capabilities
-      },
-    }) do
-      require('lspconfig')[ls].setup(cfg)
-    end
+      }) do
+        require('lspconfig')[ls].setup(cfg)
+      end
+    end },
+
+  { 'simrat39/symbols-outline.nvim', config = function()
+    require('symbols-outline').setup()
   end
+  }
 }
 
 -- START COPYPASTA https://github.com/neovim/neovim/commit/5b04e46d23b65413d934d812d61d8720b815eb1c
