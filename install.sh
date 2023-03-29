3d4c929407366580c74f1c587f6322be1908fa16
Всратослав Бурый <3831206+vsratobury@users.noreply.github.com>
2023-03-29 12:14:43 +0000

    Исправил ошибки в последовательности установки

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 # setup mac ports;
 curl -L -o macports.pkg https://github.com/macports/macports-base/releases/download/v2.6.4/MacPorts-2.6.4-10.13-HighSierra.pkg && open macports.pkg
+echo "Wait for installing of Macport completed"
+read -n 1 -s -r -p "Press any key to continue"
+
 port selfupdate
 
 # setup gnu pgp2 whit pinentry_mac
@@ -9,17 +12,12 @@ port install gnupg2 +pinentry_mac
 
 # compile kitty
 port install python39 pkgconfig libpng lcms2 librsync harfbuzz
-port select --set python python39
-port select --set python3 python39
-
-cd $TMPDIR && git clone https://github.com/kovidgoyal/kitty && cd kitty
-CFLAGS=-I/opt/local/include LDFLAGS=-L/opt/local/lib make app
-cp -R ./kitty.app /Applications/
+port select --set python python39 && port select --set python3 python39
 
 # setup fonts
 cd -
 mkdir .fonts
-curl -L -o hack-font.zip https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+curl -L -o Fira_Code_v6.2.zip https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
 unzip -j Fira_Code_v6.2.zip -d .fonts/ && rm Fira_Code_v6.2.zip
 fc-cache -fv
 
@@ -31,14 +29,18 @@ port  install the_silver_searcher
 echo "export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g \"\"'" >> .bashrc
 mv ~/.bashrc ~/.profile
 
+cd $TMPDIR && git clone https://github.com/kovidgoyal/kitty && cd kitty
+CFLAGS=-I/opt/local/include LDFLAGS=-L/opt/local/lib make app
+cp -R ./kitty.app /Applications/
+
 # compile neovim
-port install ninja cmake autoconf automake
+port install ninja cmake autoconf automake gettext xsel
 cd $TMPDIR
 git clone https://github.com/neovim/neovim.git && cd neovim
 make install CMAKE_BUILD_TYPE=Release
 
 # setup fish shell
-port install fish && chpass -s /opt/local/bin/fish ${USER}
+port install fish && chpass -s /opt/local/bin/fish ${USER} && sudo sh -c 'echo /opt/local/bin/fish >> /etc/shells'
 # curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # install go and others
