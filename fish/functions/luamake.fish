c780ae3614b83e0de1f8710d2edf4219c47240a8
Всратослав Бурый <3831206+vsratobury@users.noreply.github.com>
2023-11-27 11:33:24 +0300

    Remove old config files for nvim

---
+++ fish/functions/luamake.fish
@@ -0,0 +1,4 @@
+function luamake --wraps=/Users/vsratobury/.lua/luamake/luamake --description 'alias luamake=/Users/vsratobury/.lua/luamake/luamake'
+  /Users/vsratobury/.lua/luamake/luamake $argv
+        
+end
